f0d1c710db9a78ed27b583baca22a2759c5c4fc8
Sheena Donnelly <sheena.donnelly@jacksonriver.com>
2017-03-21 15:08:41 -0400

    Adding theming for fullPage functionality.

--- js/feature_page.js
+++ js/feature_page.js
@@ -1,5 +1,28 @@
 (function($) {
   $(document).ready(function() {
-    $('a.toolbar-icon').removeAttr('title');
+    $('#fullpage').fullpage({
+      //Navigation
+      menu: '#menu',
+      lockAnchors: false,
+      navigation: false,
+
+      //Scrolling
+      css3: true,
+      scrollingSpeed: 700,
+      autoScrolling: true,
+      fitToSection: true,
+
+      //Design
+      controlArrows: true,
+      verticalCentered: true,
+      fixedElements: '#header, .footer',
+
+      //Custom selectors
+      sectionSelector: '.section',
+      slideSelector: '.slide',
+
+      lazyLoading: true,
+
+    });
   });
 })(jQuery);
